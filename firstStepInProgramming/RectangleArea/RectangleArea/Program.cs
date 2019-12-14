21a4a642430f080978c1810c3ca94be8e553d279
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-14 15:45:34 +0300

    just upload some fills

---
+++ firstStepInProgramming/RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter heith");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter witgh");
+            double b = double.Parse(Console.ReadLine());
+            double result = a * b;
+            Console.WriteLine("your result is "+result);
+        }
+    }
+}
